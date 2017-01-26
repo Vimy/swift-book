3bac43803ea5a89e96a5aa6e9abb7ffc49910dca
Matthias Vermeulen <Matthias@MacBook-Pro-van-Matthias-2.local>
2017-01-26 17:42:57 +0100

    conditionals

--- BNR.playground/Contents.swift
+++ BNR.playground/Contents.swift
@@ -4,3 +4,9 @@ import UIKit
 
 var str = "Hello, Joren"
 let string2 = "Dag github"
+let townName: String = "Menen"
+let numberOfStopLights: Int = 4
+var population: Int
+population = 333334
+let townDescription = "\(townName) has a population of \(population) and \(numberOfStopLights) stoplights"
+print (townDescription)
---
+++ Conditionals.playground/Contents.swift
@@ -0,0 +1,19 @@
+
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var population: Int = 5422
+var message: String
+
+if (population < 10000)
+{
+    message = "\(population) is a small town"
+}
+else
+{
+    message = "\(population) is pretty big!"
+}
+
+print(message)
+
