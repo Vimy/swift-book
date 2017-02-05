ee90aa6ca41106c346001725dde38d38696bcc32
Matthias Vermeulen <Matthias@MacBook-Pro-van-Matthias-2.local>
2017-02-05 02:34:40 +0100

    functions

---
+++ Functions.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+
+func printGreeting()
+{
+    print("Hello jongeman")
+}
+
+printGreeting()
+
+func personalPrintGreeting(name: String)
+{
+    print("Hello \(name)")
+}
+
+personalPrintGreeting(name: "Jos")
+
